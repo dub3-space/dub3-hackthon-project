f6b4cc640c7b87f773961a7a29205662eb42d9b3
Fabriziogianni7 <fabrizio@keyko.io>
2023-11-18 15:53:36 +0300

    hooks

---
+++ packages/nextjs/hooks/scaffold-eth/dub3/utils.ts
@@ -0,0 +1,12 @@
+export const ContractsAndAbis = {
+  SpeakerNFT: {
+    address: "0xB31B82CDF32ce766E7acB943565347383Ac9ec26",
+    abi: [],
+  },
+};
+
+export type UseMintSpeakerNFT = {
+  contractName: "SpeakerNFT";
+  functionName?: string;
+  args?: any[];
+};
