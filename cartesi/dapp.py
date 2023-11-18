a02dc2972f39ae8ff85bff5e8e9ceee23f3d912c
mrpsycox <simone.silv@yahoo.com>
2023-11-18 23:08:59 +0300

    adding new version of dapp to handle better sample rate

--- cartesi/dapp.py
+++ cartesi/dapp.py
@@ -19,83 +19,103 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
+def str2hex(str):
+    """
+    Encodes a string as a hex string
+    """
+    return "0x" + str.encode("utf-8").hex()
+
 def handle_advance(data):
-    logger.info(f"Received advance request data {data}")
-    logger.info(data["payload"])
+    try: 
+        logger.info(f"Received advance request data {data}")
+        logger.info(data["payload"])
 
-    payload = bytes.fromhex(data["payload"][2:]).decode('utf-8')
+        payload = bytes.fromhex(data["payload"][2:]).decode('utf-8')
 
-    logger.info('decoded payload\n' + payload)
+        logger.info('decoded payload\n' + payload)
 
-    parsed_data = json.loads(payload)
+        parsed_data = json.loads(payload)
 
-    sample_url = parsed_data["sample"]
+        sample_url = parsed_data["sample"]
 
-    logger.info(sample_url)
+        logger.info(sample_url)
 
-    response = requests.get(sample_url)
+        response = requests.get(sample_url)
 
-    if response.status_code == 200:
-    # Get the content of the response
-        audio_content = response.content
-        
-        file_path = "downloaded_audio.wav"  # Change the file extension if the audio is in a different format
-        
-        # Save the downloaded file
-        with open(file_path, "wb") as file:
-            file.write(audio_content)
+        if response.status_code == 200:
+        # Get the content of the response
+            audio_content = response.content
+            
+            file_path = "downloaded_audio.wav"  # Change the file extension if the audio is in a different format
+            
+            # Save the downloaded file
+            with open(file_path, "wb") as file:
+                file.write(audio_content)
 
-        logger.info(f"File saved as '{file_path}'")
+            logger.info(f"File saved as '{file_path}'")
+
+            try:
+                with wave.open(file_path, "rb") as wave_file:
+                    frame_rate = wave_file.getframerate()
+                    logger.info(f"Frame rate of input audio: {frame_rate}")
+                    num_channels = wave_file.getnchannels()
+                    logger.info(num_channels)
+            except Exception as e:
+                logger.error(f"Cannot detect frame rate from input audio.... Error: {e}")
+
+        else:
+            logger.error("Failed to download the file.")
+
+        speech_data = parsed_data['speech']
+
+        logger.info('This is a speech ' + speech_data)
+
+        config = XttsConfig()
+        config.load_json("model/config.json")
+        model = Xtts.init_from_config(config)
+        model.load_checkpoint(config, checkpoint_dir="model/", eval=True)
 
         try:
-            with wave.open(file_path, "rb") as wave_file:
-                frame_rate = wave_file.getframerate()
-                logger.info(f"Frame rate of input audio: {frame_rate}")
+            outputs = model.synthesize(
+                speech_data,
+                config,
+                speaker_wav="downloaded_audio.wav",
+                language='it'
+            )
+
+            scaled = np.int16(outputs['wav'] * 32767)
+
+            sample_rate = 44100
+            output_sample_rate = sample_rate // 2
+
+            write('output.wav', output_sample_rate, scaled)
+
+            try:
+                with wave.open('output.wav', "rb") as wave_file:
+                    frame_rate = wave_file.getframerate()
+                    logger.info(f"Frame rate of input audio: {frame_rate}")
+                    num_channels = wave_file.getnchannels()
+                    logger.info(num_channels)
+            except Exception as e:
+                logger.error(f"Cannot detect frame rate from input audio.... Error: {e}")
+
         except Exception as e:
-            logger.error(f"Cannot detect frame rate from input audio.... Error: {e}")
+            logger.error(e)
 
-    else:
-        logger.error("Failed to download the file.")
+        api_key = os.environ.get("PINATA_API_KEY")
+        secret_key = os.environ.get("PINATA_SECRET_API_KEY")
+        if api_key and secret_key:
+            pinata = PinataPy(api_key, secret_key)
+            resp = pinata.pin_file_to_ipfs(f'output.wav')
+            print(resp['IpfsHash'])
 
-    speech_data = parsed_data['speech']
+        payloadToSent = resp['IpfsHash']
+        response = requests.post(rollup_server + "/notice", json={"payload": str2hex(payloadToSent)})
+        logger.info(f"Received notice status {response.status_code} body {response.content}")
 
-    logger.info('This is a speech ' + speech_data)
-
-    config = XttsConfig()
-    config.load_json("model/config.json")
-    model = Xtts.init_from_config(config)
-    model.load_checkpoint(config, checkpoint_dir="model/", eval=True)
-
-    try:
-        outputs = model.synthesize(
-            speech_data,
-            config,
-            speaker_wav="downloaded_audio.wav",
-            gpt_cond_len=3,
-            language="en",
-        )
-        scaled = np.int16(outputs['wav'] * 32767)
-
-        sample_rate = 44100
-
-        write('output.wav', sample_rate // 2, scaled)
+        return "accept"
     except Exception as e:
         logger.error(e)
-
-    api_key = os.environ.get("PINATA_API_KEY")
-    secret_key = os.environ.get("PINATA_SECRET_API_KEY")
-    if api_key and secret_key:
-        pinata = PinataPy(api_key, secret_key)
-        resp = pinata.pin_file_to_ipfs('output.wav')
-        print(resp['IpfsHash'])
-
-    # notice = {
-    #     "output": 
-    # }
-    # response = requests.post(rollup_server + "/notice", json=finish)
-
-    #Then, give the output! Create a Notice for that
-    return "accept"
 
 def handle_inspect(data):
     logger.info(f"Received inspect request data {data}")
