68399bbdde8ba8deb7310cd31dde9c422e96f65b
andrea <andrea.batino@oracle.com>
2023-11-18 23:50:28 +0300

    ccip integration

--- packages/nextjs/hooks/dub3/utils.ts
+++ packages/nextjs/hooks/dub3/utils.ts
@@ -1,21 +1,24 @@
-import { abi } from "./SpeakerNFTAbi";
+import { abi as buyerAbi } from "./BuyerNFTABI";
+import { abi as speakerAbi } from "./SpeakerNFTAbi";
+import { abi as destinationMinterABI } from "./destinationMinterABI";
+import { abi as sourceMinterAbi } from "./sourceMinterABI";
 
 export const ContractsAndAbis = {
   SpeakerNFT: {
     address: "0xB31B82CDF32ce766E7acB943565347383Ac9ec26",
-    abi: abi,
+    abi: speakerAbi,
   },
   BuyerNFT: {
     address: "0x59087174Dd6F511C73e62618DaeE710F2db0819F",
-    abi: abi,
+    abi: buyerAbi,
   },
   SourceMinter: {
     address: "0x7e28Bca799F90AB6e8264E93B123cE9990987567",
-    abi: abi,
+    abi: sourceMinterAbi,
   },
   DestinationMinter: {
     address: "0xF0228036FC21aD442250d4c887F492c50b5A3A37",
-    abi: abi,
+    abi: destinationMinterABI,
   },
 };
 
