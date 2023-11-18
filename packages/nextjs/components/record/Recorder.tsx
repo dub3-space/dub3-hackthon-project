eea908ba565c714cec1d4b18e8895c6c51cfab2c
Fabriziogianni7 <fabrizio@keyko.io>
2023-11-18 18:09:13 +0300

    mint speaker nft

--- packages/nextjs/components/record/Recorder.tsx
+++ packages/nextjs/components/record/Recorder.tsx
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import lighthouse from "@lighthouse-web3/sdk";
 import { LoaderIcon } from "react-hot-toast";
+import { useAccount, useContractWrite } from "wagmi";
+import { ContractsAndAbis } from "~~/hooks/dub3/utils";
+import { notification } from "~~/utils/scaffold-eth";
 
 export const Recorder = () => {
   const [file, setFile] = useState<FileList | null>();
   const [isUploadLoading, setIsUploadLoading] = useState<boolean>();
   const [fileUploaded, setFileUploaded] = useState<boolean>();
   const [price, setPrice] = useState("");
+  const [cid, setCid] = useState("");
+  const { address: to, isDisconnected } = useAccount();
+
+  const SAMPLE_TEXT =
+    " Unlock new possibilities with cutting-edge technology. Experience innovation at your fingertips. Embrace                the future today.";
+
+  // const { ...writeHook } = useDub3Contract({ contractName: "SpeakerNFT", functionName: "mint" });
+  const { data, isSuccess, write } = useContractWrite({
+    address: ContractsAndAbis.SpeakerNFT.address,
+    abi: ContractsAndAbis.SpeakerNFT.abi,
+    functionName: "mint",
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      notification.success(`Transaction with hash ${data?.hash} completed successfully!`, {
+        icon: "🎉",
+      });
+    }
+  }, [data?.hash, isSuccess]);
+
   const progressCallback = (progressData: { total: number; uploaded: number }) => {
     const percentageDone = 100 - Number(progressData?.total / progressData?.uploaded);
     console.log(percentageDone);
@@ -28,11 +52,22 @@ export const Recorder = () => {
     console.log("File Status:", output);
 
     console.log("Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash);
+    setCid(output.data.Hash);
     setIsUploadLoading(false);
     setFileUploaded(true);
     alert(`File uploaded at ${"https://gateway.lighthouse.storage/ipfs/"}`);
   };
 
+  const handleMint = async () => {
+    if (isDisconnected) {
+      alert("connect wallet first");
+      return;
+    }
+    console.log(ContractsAndAbis.SpeakerNFT.abi);
+    write({
+      args: [to, cid, SAMPLE_TEXT, 0], // to, cid,audioscript,audioprice
+    });
+  };
   return (
     <div className="p-4">
       {fileUploaded ? ( //TODO remove, jsut for developmnenrt
@@ -45,7 +80,7 @@ export const Recorder = () => {
               onChange={e => setPrice(e.target.value)}
               className="mt-4 p-2 border border-gray-300 rounded"
             />
-            <button onClick={() => alert("CAZZO")} className="mt-2 bg-blue-500 text-white p-2 rounded">
+            <button onClick={() => handleMint()} className="mt-2 bg-blue-500 text-white p-2 rounded">
               Submit
             </button>
           </div>
--- packages/nextjs/hooks/dub3/utils.ts
+++ packages/nextjs/hooks/dub3/utils.ts
@@ -1,7 +1,9 @@
+import { abi } from "./SpeakerNFTAbi";
+
 export const ContractsAndAbis = {
   SpeakerNFT: {
     address: "0xB31B82CDF32ce766E7acB943565347383Ac9ec26",
-    abi: [],
+    abi: abi,
   },
 };
 
@@ -10,3 +12,8 @@ export type UseMintSpeakerNFT = {
   functionName?: string;
   args?: any[];
 };
+
+export enum SpeakerNFTMethods {
+  MINT = "mint",
+  MINT_CROSSCHAIN = "mint",
+}
