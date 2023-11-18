967347f0b2591aedf883ba5798424671c6232729
mrpsycox <simone.silv@yahoo.com>
2023-11-18 23:52:58 +0300

    pushing tests

---
+++ cartesi/tests/test.sh
@@ -0,0 +1,10 @@
+# Sample voice
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/Qmc5BgWR3b5juxbdNGr9Ez56q1T1x63yFJY4SjEpMT8yDQ\",\"speech\": \"Unlock new possibilities with cutting-edge technology. Experience innovation at your fingertips. Embrace the future today.\",\"output_lang\":\"en\"}"
+# Fabrizio voice
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/QmcRXYN3kNHYyxkJHYEvd84twchVyc8N2oNa4j7vMwR1om\",\"speech\": \"Unlock new possibilities with cutting-edge technology. Experience innovation at your fingertips. Embrace the future today.\",\"output_lang\":\"it\"}"
+# mrpsycox.eth voice
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/QmU68C2yNfG79mLDcXd1eRPKika7crJLcGLXUvYCD7imBf\",\"speech\": \"Unlock new possibilities with cutting-edge technology. Experience innovation at your fingertips. Embrace the future today.\",\"output_lang\":\"it\"}"
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/QmU68C2yNfG79mLDcXd1eRPKika7crJLcGLXUvYCD7imBf\",\"speech\": \"Voglio andare al mare.\",\"output_lang\":\"it\"}"
+# fante voice
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/QmYviBUyunwvgj8aTsrWfzqcdfTnfr4PyMjgYutETp2D3g\",\"speech\": \"I'm Fante, I'm living in Gran Canaria\"\",\"output_lang\":\"es\"}"
+sunodo send generic --input-encoding=string --input="{\"sample\": \"https://gateway.lighthouse.storage/ipfs/QmYviBUyunwvgj8aTsrWfzqcdfTnfr4PyMjgYutETp2D3g\",\"speech\": \"Hola, que tal? Soy el fante!\"\",\"output_lang\":\"es\"}"
