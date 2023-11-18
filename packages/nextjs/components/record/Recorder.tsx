d178a5337cece5439fc242a130407c6470de3493
Fabriziogianni7 <fabrizio@keyko.io>
2023-11-18 14:51:17 +0300

    upload sample to ipfs

--- packages/nextjs/components/record/Recorder.tsx
+++ packages/nextjs/components/record/Recorder.tsx
@@ -1,43 +1,69 @@
-import React from "react";
-import { AudioRecorder } from "react-audio-voice-recorder";
+import React, { useState } from "react";
+import lighthouse from "@lighthouse-web3/sdk";
+import { LoaderIcon } from "react-hot-toast";
 
 export const Recorder = () => {
-  const addAudioElement = (blob: Blob) => {
-    console.log("QUI");
-    const url = URL.createObjectURL(blob);
-    const audio = document.createElement("audio");
-    audio.src = url;
-    audio.controls = true;
-    document.body.appendChild(audio);
-    // const link = document.createElement("a");
-    // link.href = audio.src;
-    // link.download = "test";
-    // link.click();
+  const [file, setFile] = useState<FileList | null>();
+  const [isUploadLoading, setIsUploadLoading] = useState<boolean>();
+  const [fileUploaded, setFileUploaded] = useState<boolean>();
+  const [price, setPrice] = useState("");
+  const progressCallback = (progressData: { total: number; uploaded: number }) => {
+    const percentageDone = 100 - Number(progressData?.total / progressData?.uploaded);
+    console.log(percentageDone);
+  };
+
+  const uploadFile = async (file: FileList | null | undefined) => {
+    setIsUploadLoading(true);
+    if (!file) {
+      alert("upload file first");
+      return;
+    }
+    const output = await lighthouse.upload(
+      file,
+      process.env.NEXT_PUBLIC_LIGHTHOUSE_KEY as string,
+      false,
+      undefined,
+      progressCallback,
+    );
+    console.log("File Status:", output);
+
+    console.log("Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash);
+    setIsUploadLoading(false);
+    setFileUploaded(true);
+    alert(`File uploaded at ${"https://gateway.lighthouse.storage/ipfs/"}`);
   };
 
   return (
-    <div>
-      <AudioRecorder
-        onRecordingComplete={addAudioElement}
-        audioTrackConstraints={{
-          noiseSuppression: true,
-          echoCancellation: true,
-          // autoGainControl,
-          // channelCount,
-          // deviceId,
-          // groupId,
-          // sampleRate,
-          // sampleSize,
-        }}
-        onNotAllowedOrFound={err => console.table("cazzo" + err)}
-        downloadOnSavePress={true}
-        downloadFileExtension="webm"
-        mediaRecorderOptions={{
-          audioBitsPerSecond: 128000,
-        }}
-        showVisualizer={true}
-      />
-      <br />
+    <div className="p-4">
+      {fileUploaded || true ? ( //TODO remove, jsut for developmnenrt
+        <>
+          <div className="flex flex-col items-center justify-center max-w-sm text-black">
+            <input
+              type="number"
+              placeholder={`Enter Price (USDC)`}
+              value={price}
+              onChange={e => setPrice(e.target.value)}
+              className="mt-4 p-2 border border-gray-300 rounded"
+            />
+            <button onClick={() => alert("CAZZO")} className="mt-2 bg-blue-500 text-white p-2 rounded">
+              Submit
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <input onChange={e => setFile(e.target.files)} type="file" className="mt-4" />
+          {isUploadLoading ? (
+            <div className="flex items-center justify-center">
+              <LoaderIcon />
+            </div>
+          ) : (
+            <button className="btn btn-secondary btn-sm" onClick={() => uploadFile(file)}>
+              Upload
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 };
